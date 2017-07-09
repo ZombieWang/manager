a1d7b3b2f62c99fcf0d22693c33230ee266240bf
Frank <frank7300228@gmail.com>
2017-07-09 22:59:58 +0800

    finish session 111

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,11 @@
+import { EMAIL_CHANGED } from '../actions/types'
+
 const INITAL_STATE = { email: '' }
 
 export default (state = INITAL_STATE, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+      console.log('action!')
     default:
       return state
   }
