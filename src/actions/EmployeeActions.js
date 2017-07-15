8aec87a001f51deb63460b3c9e4729b290ee3863
Frank <frank7300228@gmail.com>
2017-07-15 21:52:30 +0800

    finish session 144

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase'
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types'
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
+} from './types'
 import { Actions } from 'react-native-router-flux'
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -17,6 +21,16 @@ export const employeeCreate = ({ name, phone, shift }) => {
       .then(() => {
         dispatch({ type: EMPLOYEE_CREATE })
         Actions.employeeList({ type: 'reset' })
+      })
+  }
+}
+
+export const employeesFetch = () => {
+  return (dispatch) => {
+    const { currentUser } = firebase.auth()
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() })
       })
   }
 }
